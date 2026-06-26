3c94aa94420e841063ecb66ffb0291fa05c97537
doubleO-G <64032198+doubleO-G@users.noreply.github.com>
2026-06-27 01:29:42 +0300

    Update index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,6 +1,6 @@
 import { createFileRoute } from "@tanstack/react-router";
 import { useEffect, useMemo, useState } from "react";
-import bannerAsset from "@/assets/banner.asset.json";
+import bannerAsset from "@/assets/banner.jpeg";
 import blogoAsset from "@/assets/blogo.png";
 import tshirtBlack from "@/assets/tshirt-black.jpg";
 import tshirtWhite from "@/assets/tshirt-white.jpg";
